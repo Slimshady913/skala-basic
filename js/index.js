c86146a23d406a2f980372b4dd9a62b75039f100
최태성 <slimshady913@naver.com>
2026-01-20 15:13:14 +0900

    object

---
+++ js/index.js
@@ -0,0 +1,4 @@
+        console.log("Hello, World!");
+        console.error("This is an error message.");
+        document.write("<p>JavaScript가 정상적으로 작동하고 있습니다!</p>");
+        document.write("아무런 문장이나 작성");
